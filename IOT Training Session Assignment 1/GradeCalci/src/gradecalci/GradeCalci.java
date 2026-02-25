46e5e3266bb5426580a7b24c664b6a88c37dfc1e
Sinchanabhat23 <sinchanabhat23@gmail.com>
2026-02-25 22:19:35 +0530

    Add files via upload

---
+++ IOT Training Session Assignment 1/GradeCalci/src/gradecalci/GradeCalci.java
@@ -0,0 +1,26 @@
+package gradecalci;
+import java.util.*;
+public class GradeCalci {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int marks=sc.nextInt();
+      switch(marks / 10) {
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+        }
+    }
+}
+    
